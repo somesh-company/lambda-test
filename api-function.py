c4c4292c98b6c45e11abc29f047442a9be96700b
somesh-B <somesh220f@gmail.com>
2025-11-11 22:11:24 +0530

    changed lambda function

--- api-function.py
+++ api-function.py
@@ -7,6 +7,6 @@ def lambda_handler(event, context):
     response = requests.get('https://regres.in/api/users?page=2')
     data = response.json()
     return {
-        'statusCode': 200,
-        'body': json.dumps({'message': 'API Call Successful','data': data['entries'][:5]})
+        'statusCode': response.status_code,
+        'body': data
     }
