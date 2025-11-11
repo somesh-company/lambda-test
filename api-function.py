f87e6d3148575e2d4c233671ef99c70a50e2e25c
somesh-B <somesh220f@gmail.com>
2025-11-11 22:29:27 +0530

    added correct lambda python function

--- api-function.py
+++ api-function.py
@@ -5,8 +5,7 @@ import requests # Example dependency
 def lambda_handler(event, context):
 
     response = requests.get('https://regres.in/api/users?page=2')
-    data = response.json()
     return {
         'statusCode': response.status_code,
-        'body': data
+        'body': response.text[:1000]
     }
